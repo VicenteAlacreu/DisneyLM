e3eee95d065cace0c69c50b88033e1b012484bab
JorgeEscrichCIEEP <jorge.escrichlaorden@cieep.com>
2024-05-02 20:22:44 +0200

    Añade Componente Inicio y Personajes

--- DisneyAP/src/app/app.routes.ts
+++ DisneyAP/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { PersonajesComponent } from './Componentes/personajes/personajes.component';
 
-export const routes: Routes = [];
+
+    export const routes: Routes = [
+        { path: '', component: InicioComponent }, // Ruta para la página de inicio
+        { path: 'personajes', component: PersonajesComponent }, // Ruta para la página de personajes
+        // Otras rutas aquí...
+      ];
+    
