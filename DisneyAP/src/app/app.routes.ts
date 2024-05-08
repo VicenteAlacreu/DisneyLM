8cae5d7d4df34aa4b653859eff41ac9b836cab01
VicenteAlacreu <vicente.alacreusilvestre@cieep.com>
2024-05-08 17:41:27 +0200

    anyadir login + register

--- DisneyAP/src/app/app.routes.ts
+++ DisneyAP/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { InicioComponent } from './Componentes/inicio/inicio.component';
 import { PersonajesComponent } from './Componentes/personajes/personajes.component';
 import { PersonajeComponent } from './Componentes/personaje/personaje.component';
 import { ErrorComponent } from './Componentes/error/error.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { RegisterComponent } from './Componentes/register/register.component';
 
 
     export const routes: Routes = [
@@ -10,6 +12,8 @@ import { ErrorComponent } from './Componentes/error/error.component';
         { path: 'inicio', component: InicioComponent}, // Ruta para la página de inicio
         { path: 'personajes', component: PersonajesComponent },// Ruta para la página de personajes
         { path: 'personajes/:id', component: PersonajeComponent},// Ruta para la página de personaje con id
+        { path: 'login', component:LoginComponent}, //Ruta para la pagina de login
+        { path: 'register', component: RegisterComponent}, //Ruta para la pagina de registrarse
         
         
         { path: "**", component: ErrorComponent}
